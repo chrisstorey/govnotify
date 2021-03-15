e878a8b7821aa4d653aeccb5e46abf7ff61f3d4a
Chris Storey <chris@chrisstorey.co.uk>
2021-03-15 21:11:21 +0000

    update from xps13

--- download_notify.py
+++ download_notify.py
@@ -1,10 +1,11 @@
-from notifications_python_client import NotificationsAPIClient
-import yaml
 import csv
 
+import yaml
+from notifications_python_client import NotificationsAPIClient
+
 # Get config data
-with open("config.yml", "r") as ymlfile:
-    cfg = yaml.load(ymlfile, Loader=yaml.SafeLoader)
+with open("config.yml", "r") as yamlfile:
+    cfg = yaml.load(yamlfile, Loader=yaml.SafeLoader)
 APIKEY = cfg["api_key"]
 
 # Setup notify client and get failures
@@ -27,58 +28,57 @@ with open("downloaded.csv", "w", newline='') as csvfilewriter:
 
     # Parse data to isolate details from body and subject lines
     for i in range(len(response['notifications'])):
-            originalEmailAddress = response['notifications'][i]['email_address']
-            print(originalEmailAddress)
-            subject = response['notifications'][i]['subject']
-            appIdStart = subject.find("ID")
-            appId = subject[appIdStart+3:appIdStart+13]
-            print(appId)
+        originalEmailAddress = response['notifications'][i]['email_address']
+        print(originalEmailAddress)
+        subject = response['notifications'][i]['subject']
+        appIdStart = subject.find("ID")
+        appId = subject[appIdStart + 3:appIdStart + 13]
+        print(appId)
 
-            claimantNameEnd = subject.find(",", appIdStart+15)
-            claimantName = subject[appIdStart+15:claimantNameEnd]
-            print(claimantName)
+        claimantNameEnd = subject.find(",", appIdStart + 15)
+        claimantName = subject[appIdStart + 15:claimantNameEnd]
+        print(claimantName)
 
-            vacancyTitle = subject[claimantNameEnd+2:]
-            print(vacancyTitle)
+        vacancyTitle = subject[claimantNameEnd + 2:]
+        print(vacancyTitle)
 
-            body = response['notifications'][i]['body']
-            vacancyHolderNameStart = body.find("Dear")+5
-            vacancyHolderNameEnd = body.find("\r\n", vacancyHolderNameStart)
-            vacancyHolderName = body[vacancyHolderNameStart:vacancyHolderNameEnd]
-            print(vacancyHolderName)
+        body = response['notifications'][i]['body']
+        vacancyHolderNameStart = body.find("Dear") + 5
+        vacancyHolderNameEnd = body.find("\r\n", vacancyHolderNameStart)
+        vacancyHolderName = body[vacancyHolderNameStart:vacancyHolderNameEnd]
+        print(vacancyHolderName)
 
-            introductionIdStart = body.find("Introduction ID:")+17
-            introductionIdEnd = introductionIdStart+9
-            introductionId = body[introductionIdStart:introductionIdEnd]
-            print(introductionId)
+        introductionIdStart = body.find("Introduction ID:") + 17
+        introductionIdEnd = introductionIdStart + 9
+        introductionId = body[introductionIdStart:introductionIdEnd]
+        print(introductionId)
 
-            employerJobReferenceStart = body.find("your job reference:")+20
-            employerJobReferenceEnd = body.find("\r\n", employerJobReferenceStart)
-            employerJobReference = body[employerJobReferenceStart:employerJobReferenceEnd]
-            print(employerJobReference)
+        employerJobReferenceStart = body.find("your job reference:") + 20
+        employerJobReferenceEnd = body.find("\r\n", employerJobReferenceStart)
+        employerJobReference = body[employerJobReferenceStart:employerJobReferenceEnd]
+        print(employerJobReference)
 
-            vacancyLocationCityStart = body.find("location:")+10
-            vacancyLocationCityEnd = body.find("\r\n", vacancyLocationCityStart)
-            vacancyLocationCity = body[vacancyLocationCityStart:vacancyLocationCityEnd]
-            print(vacancyLocationCity)
+        vacancyLocationCityStart = body.find("location:") + 10
+        vacancyLocationCityEnd = body.find("\r\n", vacancyLocationCityStart)
+        vacancyLocationCity = body[vacancyLocationCityStart:vacancyLocationCityEnd]
+        print(vacancyLocationCity)
 
-            vacancyLocationPostcodeStart = body.find("postcode:")+10
-            vacancyLocationPostcodeEnd = body.find("\r\n", vacancyLocationPostcodeStart)
-            vacancyLocationPostcode = body[vacancyLocationPostcodeStart:vacancyLocationPostcodeEnd]
-            print(vacancyLocationPostcode)
-            sentAtTime = response['notifications'][i]['sent_at']
-            print(sentAtTime)
+        vacancyLocationPostcodeStart = body.find("postcode:") + 10
+        vacancyLocationPostcodeEnd = body.find("\r\n", vacancyLocationPostcodeStart)
+        vacancyLocationPostcode = body[vacancyLocationPostcodeStart:vacancyLocationPostcodeEnd]
+        print(vacancyLocationPostcode)
+        sentAtTime = response['notifications'][i]['sent_at']
+        print(sentAtTime)
 
-            # write line
-            notification_writer.writerow([originalEmailAddress,
-                                          appId,
-                                          claimantName,
-                                          vacancyTitle,
-                                          vacancyHolderName,
-                                          introductionId,
-                                          employerJobReference,
-                                          vacancyLocationCity,
-                                          vacancyLocationPostcode,
-                                          sentAtTime])
+        # write line
+        notification_writer.writerow([originalEmailAddress,
+                                      appId,
+                                      claimantName,
+                                      vacancyTitle,
+                                      vacancyHolderName,
+                                      introductionId,
+                                      employerJobReference,
+                                      vacancyLocationCity,
+                                      vacancyLocationPostcode,
+                                      sentAtTime])
     csvfilewriter.close()
-
