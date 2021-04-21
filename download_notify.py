eedc916bcd57c09f817ef39369c0bebd8169f8f0
chris storey <chris.storey@working-age.digital.dwp.gov.uk>
2021-04-21 18:34:50 +0100

    Updated for new template

--- download_notify.py
+++ download_notify.py
@@ -6,79 +6,155 @@ import csv
 with open("config.yml", "r") as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.SafeLoader)
 APIKEY = cfg["api_key"]
+NEW_FORMAT = cfg["new"]
 
 # Setup notify client and get failures
 notifications_client = NotificationsAPIClient(APIKEY)
 response = notifications_client.get_all_notifications(status='failed')
 
+
 # open file and write csv column names
 with open("downloaded.csv", "w", newline='') as csvfilewriter:
     notification_writer = csv.writer(csvfilewriter, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    notification_writer.writerow(["email address",
-                                  "applicationID",
-                                  "claimantName",
-                                  "vacancyTitle",
-                                  "vacancyHolderName",
-                                  "introductionId",
-                                  "employerJobReference",
-                                  "vacancyLocationCity",
-                                  "vacancyLocationPostcode",
-                                  "sentAtTime", "status"])
+    if NEW_FORMAT == True:
+        notification_writer.writerow(["email address",
+                                      "employerName",
+                                      "applicationID",
+                                      "claimantName",
+                                      "vacancyTitle",
+                                      "vacancyHolderName",
+                                      "introductionId",
+                                      "employerJobReference",
+                                      "vacancyLocationCity",
+                                      "vacancyLocationPostcode",
+                                      "sentAtTime", "status"])
+    else:
+        notification_writer.writerow(["email address",
+                                      "applicationID",
+                                      "claimantName",
+                                      "vacancyTitle",
+                                      "vacancyHolderName",
+                                      "introductionId",
+                                      "employerJobReference",
+                                      "vacancyLocationCity",
+                                      "vacancyLocationPostcode",
+                                      "sentAtTime", "status"])
 
     # Parse data to isolate details from body and subject lines
     for i in range(len(response['notifications'])):
             originalEmailAddress = response['notifications'][i]['email_address']
             print(originalEmailAddress)
             subject = response['notifications'][i]['subject']
-            appIdStart = subject.find("ID")
-            appId = subject[appIdStart+3:appIdStart+13]
-            print(appId)
+            if NEW_FORMAT == True:
 
-            claimantNameEnd = subject.find(",", appIdStart+15)
-            claimantName = subject[appIdStart+15:claimantNameEnd]
-            print(claimantName)
+                employerNameStart = subject.find("to")
+                employerNameEnd = subject.find(",")
+                employerName = subject[employerNameStart+3:employerNameEnd]
+                print(employerName)
 
-            vacancyTitle = subject[claimantNameEnd+2:]
-            print(vacancyTitle)
+                appIdStart = subject.find("KS")
+                appId = subject[appIdStart:appIdStart+10]
+                print(appId)
 
-            body = response['notifications'][i]['body']
-            vacancyHolderNameStart = body.find("Dear")+5
-            vacancyHolderNameEnd = body.find("\r\n", vacancyHolderNameStart)
-            vacancyHolderName = body[vacancyHolderNameStart:vacancyHolderNameEnd]
-            print(vacancyHolderName)
+                claimantNameEnd = subject.find("for", appIdStart+10)
+                claimantName = subject[appIdStart+12:claimantNameEnd-1]
+                print(claimantName)
 
-            introductionIdStart = body.find("Introduction ID:")+17
-            introductionIdEnd = introductionIdStart+9
-            introductionId = body[introductionIdStart:introductionIdEnd]
-            print(introductionId)
+                vacancyTitle = subject[claimantNameEnd+4:]
 
-            employerJobReferenceStart = body.find("your job reference:")+20
-            employerJobReferenceEnd = body.find("\r\n", employerJobReferenceStart)
-            employerJobReference = body[employerJobReferenceStart:employerJobReferenceEnd]
-            print(employerJobReference)
+                print(vacancyTitle)
 
-            vacancyLocationCityStart = body.find("location:")+10
-            vacancyLocationCityEnd = body.find("\r\n", vacancyLocationCityStart)
-            vacancyLocationCity = body[vacancyLocationCityStart:vacancyLocationCityEnd]
-            print(vacancyLocationCity)
+                body = response['notifications'][i]['body']
+                vacancyHolderNameStart = body.find("Dear")+5
+                vacancyHolderNameEnd = body.find("\r\n", vacancyHolderNameStart)
+                vacancyHolderName = body[vacancyHolderNameStart:vacancyHolderNameEnd]
+                print(vacancyHolderName)
 
-            vacancyLocationPostcodeStart = body.find("postcode:")+10
-            vacancyLocationPostcodeEnd = body.find("\r\n", vacancyLocationPostcodeStart)
-            vacancyLocationPostcode = body[vacancyLocationPostcodeStart:vacancyLocationPostcodeEnd]
-            print(vacancyLocationPostcode)
-            sentAtTime = response['notifications'][i]['sent_at']
-            print(sentAtTime)
+                introductionIdStart = body.find("Introduction ID:")+17
+                introductionIdEnd = introductionIdStart+9
+                introductionId = body[introductionIdStart:introductionIdEnd]
+                print(introductionId)
+
+                employerJobReferenceStart = body.find("your job reference:")+20
+                employerJobReferenceEnd = body.find("\r\n", employerJobReferenceStart)
+                employerJobReference = body[employerJobReferenceStart:employerJobReferenceEnd]
+                print(employerJobReference)
+
+                vacancyLocationCityStart = body.find("location:")+10
+                vacancyLocationCityEnd = body.find("\r\n", vacancyLocationCityStart)
+                vacancyLocationCity = body[vacancyLocationCityStart:vacancyLocationCityEnd]
+                print(vacancyLocationCity)
+
+                vacancyLocationPostcodeStart = body.find("postcode:")+10
+                vacancyLocationPostcodeEnd = body.find("\r\n", vacancyLocationPostcodeStart)
+                vacancyLocationPostcode = body[vacancyLocationPostcodeStart:vacancyLocationPostcodeEnd]
+                print(vacancyLocationPostcode)
+                sentAtTime = response['notifications'][i]['sent_at']
+                print(sentAtTime)
+
+                notification_writer.writerow([originalEmailAddress,
+                                              employerName,
+                                              appId,
+                                              claimantName,
+                                              vacancyTitle,
+                                              vacancyHolderName,
+                                              introductionId,
+                                              employerJobReference,
+                                              vacancyLocationCity,
+                                              vacancyLocationPostcode,
+                                              sentAtTime, response['notifications'][i]['status']])
+
+            else:
+                appIdStart = subject.find("ID")
+                appId = subject[appIdStart + 3:appIdStart + 13]
+                print(appId)
+
+                claimantNameEnd = subject.find(",", appIdStart + 15)
+                claimantName = subject[appIdStart + 15:claimantNameEnd]
+                print(claimantName)
+
+                vacancyTitle = subject[claimantNameEnd + 2:]
+
+                print(vacancyTitle)
+
+                body = response['notifications'][i]['body']
+                vacancyHolderNameStart = body.find("Dear") + 5
+                vacancyHolderNameEnd = body.find("\r\n", vacancyHolderNameStart)
+                vacancyHolderName = body[vacancyHolderNameStart:vacancyHolderNameEnd]
+                print(vacancyHolderName)
+
+                introductionIdStart = body.find("Introduction ID:") + 17
+                introductionIdEnd = introductionIdStart + 9
+                introductionId = body[introductionIdStart:introductionIdEnd]
+                print(introductionId)
+
+                employerJobReferenceStart = body.find("your job reference:") + 20
+                employerJobReferenceEnd = body.find("\r\n", employerJobReferenceStart)
+                employerJobReference = body[employerJobReferenceStart:employerJobReferenceEnd]
+                print(employerJobReference)
+
+                vacancyLocationCityStart = body.find("location:") + 10
+                vacancyLocationCityEnd = body.find("\r\n", vacancyLocationCityStart)
+                vacancyLocationCity = body[vacancyLocationCityStart:vacancyLocationCityEnd]
+                print(vacancyLocationCity)
+
+                vacancyLocationPostcodeStart = body.find("postcode:") + 10
+                vacancyLocationPostcodeEnd = body.find("\r\n", vacancyLocationPostcodeStart)
+                vacancyLocationPostcode = body[vacancyLocationPostcodeStart:vacancyLocationPostcodeEnd]
+                print(vacancyLocationPostcode)
+                sentAtTime = response['notifications'][i]['sent_at']
+                print(sentAtTime)
 
             # write line
-            notification_writer.writerow([originalEmailAddress,
-                                          appId,
-                                          claimantName,
-                                          vacancyTitle,
-                                          vacancyHolderName,
-                                          introductionId,
-                                          employerJobReference,
-                                          vacancyLocationCity,
-                                          vacancyLocationPostcode,
-                                          sentAtTime, response['notifications'][i]['status']])
+                notification_writer.writerow([originalEmailAddress,
+                                              appId,
+                                              claimantName,
+                                              vacancyTitle,
+                                              vacancyHolderName,
+                                              introductionId,
+                                              employerJobReference,
+                                              vacancyLocationCity,
+                                              vacancyLocationPostcode,
+                                              sentAtTime, response['notifications'][i]['status']])
     csvfilewriter.close()
 
